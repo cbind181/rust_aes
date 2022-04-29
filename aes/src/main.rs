8fcdc004726ac8d1a6ac5340329aaa067f1a50c6
Mason Sipe <mason@localhost.localdomain>
2022-04-29 14:53:52 -0500

    able to read file contents

--- aes/src/main.rs
+++ aes/src/main.rs
@@ -27,53 +27,24 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
-use aes::Aes128;
-use block_modes::{BlockMode, Cbc};
-use block_modes::block_padding::Pkcs7;
-use hex_literal::hex;
-use std::str;
-use std::env;
-
-type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
-    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
-
-    let mut message = String::from("Hello world!");
-    let mut mykey =String::from("000102030405060708090A0B0C0D0E0F");
-
-    let args: Vec<String> = env::args().collect();
-
-        if args.len() >1 {
-            message = args[1].clone();
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("../../../python_aes/msg.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
         }
+    }
+}
 
-    if args.len() >2 {
-            mykey = args[2].clone();
-        }
-
-    println!("Message: {}",message);
-    println!("Key: {}",mykey);
-    println!("IV: f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
-
-    let plaintext=message.as_bytes();
-    let key = hex::decode(mykey).expect("Decoding failed");
-
-    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
-
-    let pos = plaintext.len();
-
-    let mut buffer = [0u8; 128];
-
-    buffer[..pos].copy_from_slice(plaintext);
-
-    let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
-
-    println!("\nCiphertext: {:?}",hex::encode(ciphertext));
-
-    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
-    let mut buf = ciphertext.to_vec();
-    let decrypted_ciphertext = cipher.decrypt(&mut buf).unwrap();
-
-    println!("\nCiphertext: {:?}",str::from_utf8(decrypted_ciphertext).unwrap());
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
